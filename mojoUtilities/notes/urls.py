5323c80f3c669b228a3b0062f72fdbc2f413fe16
Suvigya Agrawal <suvigyaagrawal@Suvigyas-MacBook-Pro.local>
2019-02-21 13:03:48 +0530

    Added login feature

--- mojoUtilities/notes/urls.py
+++ mojoUtilities/notes/urls.py
@@ -7,4 +7,7 @@ from . import views
 app_name = 'notes'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^login/$', views.userLogin, name='login'),
+	url(r'^@(?P<user_name>\w+)/dashboard/$', views.userDashboard, name='dashboard'),
+
 ]
--- mojoUtilities/notes/views.py
+++ mojoUtilities/notes/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import User, NotesEntry
 # Create your views here.
 def index(request):
 	return render(request, 'notes/index.html')
+
+
+def userLogin(request):
+    user_name = request.POST.get("user_name")
+    user_password = request.POST.get("user_password")
+    user_object = User.objects.filter(user_name = user_name)
+    possible_passwords = [i.user_password for i in user_object]
+    if password in possible_passwords:
+        return redirect('notes:dashboard', user_name=user_name)
+    else:
+        return HttpResponse("User not found")
+
+
+def userDashboard(request, user_name):
+	return render(request, 'notes/dashboard.html')
