062834486902c2ea1dbc3eb9fe6f9923c1f8023e
Suvigya Agrawal <suvigyaagrawal@Suvigyas-MacBook-Pro.local>
2019-02-21 12:43:44 +0530

    Added User and NotesEntry models

--- mojoUtilities/notes/models.py
+++ mojoUtilities/notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	user_name = models.CharField(max_length = 40, unique = True)
+	user_password = models.CharField(max_length = 40)
+
+	def __str__(self):
+		return f'{self.user_name}'
+
+class NotesEntry (models.Model):
+	user_name = models.ForeignKey(User)
+	note_title = models.CharField(max_length=40)
+	note_text = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return f'{self.note_title}'
