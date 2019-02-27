0ad35bf9fd32d6caefdbb6e972d3218e127eeae6
Shreyas KM <shreyaskm@Admins-MacBook-Pro.local>
2019-02-27 19:54:07 +0530

    WSGI file has been updated

--- mojoUtilities/mojoUtilities/wsgi.py
+++ mojoUtilities/mojoUtilities/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path='/home/shreyaskm/mojoUtilities'
+if path not in sys.path
+    sys.path.insert(0,path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mojoUtilities.settings'
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mojoUtilities.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mojoUtilities.settings")
 
 application = get_wsgi_application()
