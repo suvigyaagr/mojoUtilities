1268a54bba390da5b1a4b3e5a274f888e3f7ff1c
Shreyas KM <shreyaskm@Admins-MacBook-Pro.local>
2019-02-28 12:41:43 +0530

    Static files are removed

--- mojoUtilities/mojoUtilities/settings.py
+++ mojoUtilities/mojoUtilities/settings.py
@@ -125,10 +125,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS=(
-#     os.path.join(BASE_DIR, "notes/templates/notes")
-# )
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATIC_ROOT = "home/shreyaskm/mojoUtilities/static"
--- mojoUtilities/mojoUtilities/wsgi.py
+++ mojoUtilities/mojoUtilities/wsgi.py
@@ -10,11 +10,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 import sys
 
-path='/home/shreyaskm/mojoUtilities/mojoUtilities'
-if path not in sys.path
-    sys.path.insert(0,path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mojoUtilities.settings'
 
 
 from django.core.wsgi import get_wsgi_application
