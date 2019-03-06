c415ffaffb50f55e07405625c6dca491428ec611
Suvigya Agrawal <suvigyaagrawal@Suvigyas-MacBook-Pro.local>
2019-03-06 16:26:54 +0530

    Added user_profile url,view and template

--- mojoUtilities/notes/urls.py
+++ mojoUtilities/notes/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
 	url(r'^newSignup/$', views.userNewSignup, name='new_signup'),
 	url(r'^@(?P<user_name>\w+)/logout/$', views.userLogout, name='logout'),
 	url(r'^@(?P<user_name>\w+)/dashboard/$', views.userDashboard, name='dashboard'),
+	url(r'^@(?P<user_name>\w+)/user_profile/$', views.userProfile, name='user_profile'),
 	url(r'^@(?P<user_name>\w+)/add_note/$', views.addNote, name='add_note'),
 	url(r'^@(?P<user_name>\w+)/add_new_note/$', views.addNewNote, name='add_new_note'),
 	url(r'^zapier_input',views.hook_receiver_view,name='add_new_note'),
